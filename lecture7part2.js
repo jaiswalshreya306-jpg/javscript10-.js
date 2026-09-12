9cf4f5d088d48065b345d6c1e5d7e3882525f134
jaiswalshreya306-jpg <jaiswalshreya306@gmail.com>
2026-09-12 23:33:46 +0530

    NEVER GIVE UP

--- lecture7part2.js
+++
@@ -1,3 +0,0 @@
-constnow = new Date(2025,8,20,8,25,16,125);
-
-console.log(now);
---
+++ lecture9.js
@@ -0,0 +1,7 @@
+let marks = [10,54,98,67,40];
+arr.pop();
+console.log(arr);
+
+arr.unshift(10);
+arr.unshift(50)
+console.log(arr);
